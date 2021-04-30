c8cfc398e100b32895987da9ed8ef424d105c88c
NatasaZupanski <nzitune@gmail.com>
2021-04-30 13:27:23 -0400

    Initial Commit

---
+++ Shared/Turing_iPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  Turing_iPadApp.swift
+//  Shared
+//
+//  Created by Natasa Zupanski on 4/30/21.
+//
+
+import SwiftUI
+
+@main
+struct Turing_iPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
